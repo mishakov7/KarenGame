2cbd801dcb147cf6c0241d7826203e7dc67b14bd
Karen P <48729437+mintyrouge@users.noreply.github.com>
2020-05-25 15:57:35 -0400

    Karen_chara + animation/walk
    Introduces the character to Unity and sets up the walk animation, as well as user control using arrow keys or WASD.

---
+++ KarenGame/Assets/walk.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class walk : MonoBehaviour
+{
+
+    public float speed;
+
+    void Update()
+    {
+        transform.Translate(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed, 0.15f);
+    }
+}
